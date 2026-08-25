057d56930c8e44815025d36fd3385f25ef567ae9
Razerbeam1 <beamnonoob@gmail.com>
2026-08-25 20:43:46 +0700

    movement-wasd

---
+++ src/Player/Player.cpp
@@ -0,0 +1,69 @@
+#include "Player.h"
+
+Player::Player(float x, float y)
+{
+    position = {x, y};
+    speed = 250.0f;
+}
+
+void Player::Update(int screenWidth, int screenHeight)
+{
+    float deltaTime = GetFrameTime();
+
+    // Movement
+    if (IsKeyDown(KEY_W))
+    {
+        position.y -= speed * deltaTime;
+    }
+
+    if (IsKeyDown(KEY_S))
+    {
+        position.y += speed * deltaTime;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+        position.x -= speed * deltaTime;
+    }
+
+    if (IsKeyDown(KEY_D))
+    {
+        position.x += speed * deltaTime;
+    }
+
+    // Player size
+    const float playerSize = 40.0f;
+
+    // Boundary X
+    if (position.x < 0.0f)
+    {
+        position.x = 0.0f;
+    }
+
+    if (position.x > screenWidth - playerSize)
+    {
+        position.x = screenWidth - playerSize;
+    }
+
+    // Boundary Y
+    if (position.y < 0.0f)
+    {
+        position.y = 0.0f;
+    }
+
+    if (position.y > screenHeight - playerSize)
+    {
+        position.y = screenHeight - playerSize;
+    }
+}
+
+void Player::Draw()
+{
+    DrawRectangle(
+        (int)position.x,
+        (int)position.y,
+        40,
+        40,
+        BLUE
+    );
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include "raylib.h"
+#include "Player/Player.h"
 
 int main()
 {
-    InitWindow(800, 600, "Raylib Test");
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     SetTargetFPS(60);
+    InitWindow(
+        screenWidth,
+        screenHeight,
+        "Test-dev"
+    );
+    Player player(380, 280);
 
     while (!WindowShouldClose())
     {
+        // UPDATE
+        player.Update(screenWidth, screenHeight);
+
+        // DRAW
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Raylib Works!", 300, 280, 30, BLACK);
+        DrawText(
+            "WASD - Move",
+            20,
+            20,
+            20,
+            BLACK
+        );
+
+        DrawRectangleLines(
+            0,
+            0,
+            screenWidth,
+            screenHeight,
+            BLACK
+        );
+
+        player.Draw();
 
         EndDrawing();
     }
